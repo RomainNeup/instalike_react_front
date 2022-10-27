16f30c3577caa58210b700e818eb036c2e16fe46
thomas-strafrac <chipsou1807@gmail.com>
2022-10-27 17:32:50 +0200

    clsx on link

--- src/components/base/Links/Link.tsx
+++ src/components/base/Links/Link.tsx
@@ -1,10 +1,24 @@
 import { Link as ReactLink } from "react-router-dom";
+import clsx from "clsx";
 
 export function Link(props: LinkProps): JSX.Element {
+    const componentClass = clsx(
+        props.className,
+        [
+            "w-full",
+            "block",
+            "rounded-md",
+            "bg-transparent",
+            "text-primary",
+            "hover:text-primary/75",
+            "text-start",
+            "underline"
+        ],
+    )
     return (
         <div>
             <ReactLink
-                className={`${props.className} w-full block rounded-md bg-transparent text-primary hover:text-primary/75 text-start underline`}
+                className={componentClass}
                 to={props.to}
             >
                 {props.children}
