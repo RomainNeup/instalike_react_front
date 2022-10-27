4714e73cfdadc1262a5191189d34e3a9e4785540
Romain <romain.neuplanche@epitech.eu>
2022-10-27 15:54:17 +0200

    Add post to indexView

--- src/views/IndexView/IndexView.tsx
+++ src/views/IndexView/IndexView.tsx
@@ -1,5 +1,23 @@
+import { Post } from "../../components"
 import React from "react"
 
 export function IndexView(): JSX.Element {
-    return <>OK</>
+    return (
+        <div className="w-full max-w-lg content-center">
+            <Post
+                post={{
+                    id: '1',
+                    image:
+                        'https://media.istockphoto.com/photos/winding-coast-road-in-corsica-picture-id1350993173?b=1&k=20&m=1350993173&s=170667a&w=0&h=vvgGktYjPV3IWLYTvWLsQnSsDsCChR_FO3d8e7touwk=',
+                    caption: 'Mes super vacances au soleil avec mes amis',
+                    likes: 0,
+                }}
+                user={{
+                    username: 'Romain123',
+                    avatar:
+                        'https://media.istockphoto.com/photos/winding-coast-road-in-corsica-picture-id1350993173?b=1&k=20&m=1350993173&s=170667a&w=0&h=vvgGktYjPV3IWLYTvWLsQnSsDsCChR_FO3d8e7touwk=',
+                }}
+            />
+        </div>
+    )
 }
