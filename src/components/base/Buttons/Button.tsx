10e6e4fefebadf5244eb67601ab4218fdc09c97f
Romain <romain.neuplanche@epitech.eu>
2022-10-27 15:43:06 +0200

    Add className to button

--- src/components/base/Buttons/Button.tsx
+++ src/components/base/Buttons/Button.tsx
@@ -1,7 +1,7 @@
 export function Button(props: ButtonProps): JSX.Element {
     return <div>
         <button
-            className="w-full block rounded-md bg-transparent border border-primary p-2 text-primary hover:bg-primary hover:text-basic"
+            className={`${props.className} w-full block rounded-md bg-transparent border border-primary p-2 text-primary hover:bg-primary hover:text-basic`}
             type={props.type}
             onClick={props.onClick}
             disabled={props.disabled}
