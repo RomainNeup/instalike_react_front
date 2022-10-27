b0b98872f875cbd2d876c84cbf1237e56cd91472
thomas-strafrac <chipsou1807@gmail.com>
2022-10-27 17:41:41 +0200

    clsx H2

--- src/components/base/Titles/H2.tsx
+++ src/components/base/Titles/H2.tsx
@@ -1,6 +1,17 @@
+import clsx from "clsx";
+
 export function H2(props: TitleProps): JSX.Element {
+    const componentClass = clsx(
+        props.className,
+        [
+            "text-primary",
+            "text-4xl",
+            "font-extrabold",
+            "tracking-wider"
+        ],
+    )
     return (
-        <h2 className={`${props.className} text-primary text-4xl font-extrabold tracking-wider`}>
+        <h2 className={componentClass}>
             {props.children}
         </h2>
     )
