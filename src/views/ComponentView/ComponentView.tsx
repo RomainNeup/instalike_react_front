ce2674c912eec35aae8a9f8aba14c6e2d2086871
Romain <romain.neuplanche@epitech.eu>
2022-10-27 16:41:57 +0200

    Add all title to component view

--- src/views/ComponentView/ComponentView.tsx
+++ src/views/ComponentView/ComponentView.tsx
@@ -1,9 +1,13 @@
-import { H1 } from "../../components";
+import { H1, H2, H3, H4, H5 } from "../../components";
 
 export function ComponentView() {
     return (
-        <div>
+        <div className="w-full">
             <H1>Title H1</H1>
+            <H2>Title H1</H2>
+            <H3>Title H1</H3>
+            <H4>Title H1</H4>
+            <H5>Title H1</H5>
         </div>
     )
 }
