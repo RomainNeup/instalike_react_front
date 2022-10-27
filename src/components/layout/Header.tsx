9e32a1aab86677338b649cd874d17ee21e919e87
Romain <romain.neuplanche@epitech.eu>
2022-10-27 13:58:57 +0200

    implement image component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { H3 } from "../base";
+import { Image } from "../base/Images";
 
 export function Header(props: LayoutProps): JSX.Element {
     return (
@@ -8,11 +9,11 @@ export function Header(props: LayoutProps): JSX.Element {
                 <H3>InstaLike</H3>
             </Link>
             <Link to="/" className={`${props.className} w-12 h-12`}>
-                {/* <ImageRound
-        alt="PP"
-        src="https://media.istockphoto.com/photos/portrait-of-beautiful-woman-lit-by-neon-colored-lights-picture-id1313668357?b=1&k=20&m=1313668357&s=170667a&w=0&h=u-rsR__qiTfquxVEqYN2JKh5fNM7tr8okJcLU21rDKU="
-      /> */}
-                Image
+                <Image
+                    round
+                    alt="PP"
+                    src="https://media.istockphoto.com/photos/portrait-of-beautiful-woman-lit-by-neon-colored-lights-picture-id1313668357?b=1&k=20&m=1313668357&s=170667a&w=0&h=u-rsR__qiTfquxVEqYN2JKh5fNM7tr8okJcLU21rDKU="
+                />
             </Link>
         </div>
     )
