d21e0b0deb2c7e7b5e63d339192564eaaed53c81
Romain <romain.neuplanche@epitech.eu>
2022-10-27 13:37:46 +0200

    Add Button components

---
+++ src/components/base/Buttons/Button.tsx
@@ -0,0 +1,12 @@
+export function Button(props: ButtonProps): JSX.Element {
+    return <div>
+        <button
+            className="w-full block rounded-md bg-transparent border border-primary p-2 text-primary hover:bg-primary hover:text-basic"
+            type={props.type}
+            onClick={props.onClick}
+            disabled={props.disabled}
+        >
+            {props.children}
+        </button>
+    </div>
+}
---
+++ src/components/base/Buttons/type.d.ts
@@ -0,0 +1,7 @@
+interface ButtonProps {
+    children: React.ReactNode;
+    className?: string;
+    disabled?: boolean;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    type?: 'button' | 'submit' | 'reset';
+}
