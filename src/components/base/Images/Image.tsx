e2860a0c2507433998eee0156ecf4fb9da6f246a
Romain <romain.neuplanche@epitech.eu>
2022-10-28 15:59:56 +0200

    fix img component

--- src/components/base/Images/Image.tsx
+++ src/components/base/Images/Image.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import clsx from 'clsx';
 
-export function Image(props: ImageProps): JSX.Element {
+export function Image({ className, round, border, alt, src }: ImageProps): JSX.Element {
   const imageClass = clsx(
-    props.className,
+    className,
     [
       'object-cover',
       'w-full',
       'h-full',
     ],
     {
-      'rounded-lg': !props.round,
-      'rounded-full': props.round,
-      border: !!props.border,
-      'border-primary': props.border === 'primary',
-      'border-secondary': props.border === 'secondary',
-      'border-basic': props.border === 'basic',
+      'rounded-lg': !round,
+      'rounded-full': round,
+      border: !!border,
+      'border-primary': border === 'primary',
+      'border-secondary': border === 'secondary',
+      'border-basic': border === 'basic',
     },
   );
 
   return (
     <img
       className={imageClass}
-      src={props.src}
-      alt={props.alt}
+      src={src}
+      alt={alt}
     />
   );
 }
