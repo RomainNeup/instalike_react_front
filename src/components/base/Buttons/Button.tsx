5642032ef25079ea2be9437852a2d70ba43ab3d3
thomas-strafrac <chipsou1807@gmail.com>
2022-10-28 15:58:04 +0200

    esling button

--- src/components/base/Buttons/Button.tsx
+++ src/components/base/Buttons/Button.tsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import clsx from 'clsx';
 
-export function Button(props: ButtonProps): JSX.Element {
+export function Button({
+  children, className, color, disabled, fullWidth, onClick, plain, size, type,
+}: ButtonProps): JSX.Element {
   const componentClass = clsx(
-    props.className,
+    className,
     [
       'block',
       'rounded-lg',
       'border',
     ],
     {
-      'p-1 px-2': props.size === 'small',
-      'p-2': props.size === 'medium' || !props.size,
-      'p-3': props.size === 'large',
+      'p-1 px-2': size === 'small',
+      'p-2': size === 'medium' || !size,
+      'p-3': size === 'large',
 
-      'border-primary': props.color === 'primary' || !props.color,
-      'border-secondary': props.color === 'secondary',
-      'border-basic': props.color === 'basic',
+      'border-primary': color === 'primary' || !color,
+      'border-secondary': color === 'secondary',
+      'border-basic': color === 'basic',
 
-      'bg-primary text-basic hover:text-primary': (props.color === 'primary' || !props.color) && props.plain,
-      'bg-secondary text-basic hover:text-secondary': props.color === 'secondary' && props.plain,
-      'bg-basic text-primary hover:text-basic': props.color === 'basic' && props.plain,
+      'bg-primary text-basic hover:text-primary': (color === 'primary' || !color) && plain,
+      'bg-secondary text-basic hover:text-secondary': color === 'secondary' && plain,
+      'bg-basic text-primary hover:text-basic': color === 'basic' && plain,
 
-      'text-primary hover:bg-primary hover:text-basic': (props.color === 'primary' || !props.color) && !props.plain,
-      'text-secondary hover:bg-secondary hover:text-basic': props.color === 'secondary' && !props.plain,
-      'text-basic hover:bg-basic hover:text-primary': props.color === 'basic' && !props.plain,
+      'text-primary hover:bg-primary hover:text-basic': (color === 'primary' || !color) && !plain,
+      'text-secondary hover:bg-secondary hover:text-basic': color === 'secondary' && !plain,
+      'text-basic hover:bg-basic hover:text-primary': color === 'basic' && !plain,
 
-      'hover:bg-transparent': props.plain,
+      'hover:bg-transparent': plain,
 
-      'w-full': props.fullWidth,
+      'w-full': fullWidth,
     },
   );
   return (
     <button
       className={componentClass}
-      type={props.type}
-      onClick={props.onClick}
-      disabled={props.disabled}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
     >
-      {props.children}
+      {children}
     </button>
   );
 }
