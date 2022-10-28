45ea174883984e79ec5f89396da1d0ddcdd308b6
Romain <romain.neuplanche@epitech.eu>
2022-10-28 11:13:05 +0200

    Fix style

--- src/components/elements/Post/PostBody.tsx
+++ src/components/elements/Post/PostBody.tsx
@@ -4,7 +4,7 @@ export function PostBody(props: PostBodyProps): JSX.Element {
     return (
         <div className={`${props.className}`}>
             <Image className="h-96 w-full" src={props.post.image} alt={props.post.caption} border="primary" />
-            <div className="mt-2">
+            <div className="mt-4">
                 <Icon name="favorite" className="text-secondary mr-2" />
                 <Icon name="chat_bubble" className="text-secondary" />
                 <P className="font-light">{props.post.likes} j'aime</P>
