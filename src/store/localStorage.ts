2662c0a0d49fdd1b5eb1ecc6d98cb36145e5d180
Romain <romain.neuplanche@epitech.eu>
2022-11-26 17:58:12 +0100

    Remove console.log warn

--- src/store/localStorage.ts
+++ src/store/localStorage.ts
@@ -15,12 +15,8 @@ let lastUpdated: number = 0;
 
 export function saveState(state: any) {
   if (new Date().getTime() - lastUpdated > 1000) {
-    try {
-      const serializedState = JSON.stringify(state);
-      localStorage.setItem('instalike_state', serializedState);
-      lastUpdated = new Date().getTime();
-    } catch {
-      console.error('Error while saving state');
-    }
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('instalike_state', serializedState);
+    lastUpdated = new Date().getTime();
   }
 }
