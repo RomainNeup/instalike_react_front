c96f683bed8a0768df94314f3f76a8274fd5be10
Romain <romain.neuplanche@epitech.eu>
2022-12-13 16:25:33 +0100

    implement all chat components

--- src/views/ChatView.tsx
+++ src/views/ChatView.tsx
@@ -1,28 +1,39 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ChatList from '../components/elements/Chat/ChatList';
 import Icon from '../components/base/Icons/Icon';
 import Button from '../components/base/Buttons/Button';
 import P from '../components/base/Texts/P';
 import ChatBox from '../components/elements/Chat/ChatBox';
 import AddConversation from '../components/elements/Chat/AddConversation';
+import useConversationMessages from '../hooks/Message';
 
 export default function ChatView(): ReactElement {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const { id } = useParams<string>();
+  const [postMessage, messages, user] = useConversationMessages(id);
 
   return (
-    <div className="flex max-h-144 max-w-4xl overflow-hidden flex-row space-x-8">
+    <div className="flex max-h-144 h-128 max-w-4xl w-full overflow-hidden flex-row space-x-8">
+      <AddConversation open={open} close={() => setOpen(false)} />
       <div className="flex flex-col w-1/3">
-        <div className="pb-4 flex justify-between border-b border-secondary items-center">
-          <div className="h-12" />
-          <P className="text-xl">romain_nonym</P>
-          <AddConversation open={open} close={() => setOpen(false)} />
-          <Button color="primary" onClick={() => setOpen(!open)}>
-            <Icon name="edit" />
-          </Button>
+        <div className="pb-4 border-b border-secondary">
+          <div className="h-12 flex justify-between items-center">
+            <P className="text-xl">romain_nonym</P>
+            <Button color="primary" onClick={() => setOpen(!open)}>
+              <Icon name="edit" />
+            </Button>
+          </div>
         </div>
-        <ChatList className="pt-4" />
+        <ChatList className="pt-4" currentConversation={id} />
       </div>
-      <ChatBox messages={[]} user={{ _id: '', username: '', media: null }} />
+      {id && user && (
+        <ChatBox
+          messages={messages || []}
+          user={user}
+          postMessage={postMessage}
+        />
+      )}
     </div>
   );
 }
