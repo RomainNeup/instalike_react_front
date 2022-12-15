f49d7a8d688a1fd1d692c2a23c25d98c6f7fd618
Romain <romain.neuplanche@epitech.eu>
2022-12-15 17:30:45 +0100

    Create Posts hook

--- src/views/IndexView.tsx
+++ src/views/IndexView.tsx
@@ -1,33 +1,14 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement } from 'react';
+import Body from '../components/layout/Body';
 import Post from '../components/elements/Post/Post';
-import { useAppDispatch, useAppSelector } from '../store/hooks';
-import PostService from '../api/post/service';
-import { setPosts } from '../store/reducers/post/reducer';
+import usePosts from '../hooks/Posts';
 
 export default function IndexView(): ReactElement {
-  const posts = useAppSelector((state) => state.posts);
-  const { informations } = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    PostService.getPosts()
-      .then((response) => dispatch(setPosts(response.map((post) => ({
-        ...post,
-        user: {
-          ...post.user,
-          currentUser: post.user.id === informations?.id,
-        },
-        currentUser: informations?.id === post.user.id,
-        comments: post.comments.map((comment) => ({
-          ...comment,
-          currentUser: informations?.id === comment.user.id,
-        })),
-      })))));
-  }, [dispatch, informations]);
+  const { posts } = usePosts();
 
   return (
-    <div className="w-full max-w-lg content-center">
+    <Body size="medium">
       {posts.map((post) => (<Post key={post.id} post={post} />))}
-    </div>
+    </Body>
   );
 }
