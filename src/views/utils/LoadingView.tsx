4a0c6ca896b3c07581daef356745ff99796fe1df
Romain <romain.neuplanche@epitech.eu>
2022-12-19 16:40:41 +0100

    Add missing translations

--- src/views/utils/LoadingView.tsx
+++ src/views/utils/LoadingView.tsx
@@ -1,13 +1,16 @@
 import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
 import H1 from '../../components/base/Titles/H1';
 import Icon from '../../components/base/Icons/Icon';
 import Body from '../../components/layout/Body';
 
 export default function LoadingView(): ReactElement {
+  const { t } = useTranslation('common');
+
   return (
     <Body size="medium">
-      <H1 className="mb-16 text-left">Chargement...</H1>
-      <Icon name="autorenew" className="animate-spin text-8xl" color="primary" />
+      <H1 className="mb-16 text-left">{t('loading')}</H1>
+      <Icon name="autorenew" className="animate-spin text-6xl w-fit self-center p-16" color="primary" />
     </Body>
   );
 }
--- src/views/utils/NotFoundView.tsx
+++ src/views/utils/NotFoundView.tsx
@@ -1,23 +1,30 @@
 import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
 import Body from '../../components/layout/Body';
 import Button from '../../components/base/Buttons/Button';
 import P from '../../components/base/Texts/P';
 import H1 from '../../components/base/Titles/H1';
+import Icon from '../../components/base/Icons/Icon';
 
 interface NotFoundProps {
   pageName: string;
 }
 
 export default function NotFoundView({
-  pageName = 'recherché',
+  pageName = '',
 }: NotFoundProps): ReactElement {
+  const { t } = useTranslation('common');
+
   return (
     <Body size="medium">
-      <H1 className="mb-16">Page non trouvée</H1>
-      <P className="mb-8">
-        {`La page ${pageName} n'a pas été trouvée...`}
+      <H1 className="mb-16">{t('notFound.title')}</H1>
+      <P className="py-8">
+        {t('notFound.text', { page: pageName })}
       </P>
-      <Button to="/" fullWidth>Je retourne à l&apos;accueil </Button>
+      <Button to="/" fullWidth>
+        <Icon name="home" className="mr-2" />
+        {t('notFound.link')}
+      </Button>
     </Body>
   );
 }
